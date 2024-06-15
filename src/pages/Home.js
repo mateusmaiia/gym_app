4300e3f367f754d5c1eeeca91c373ad50717a071
Maia <matteusmaia889@gmail.com>
2024-06-15 14:46:00 -0300

    feat: exercise props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,7 @@ const Home = () => {
         setBodyPart={setBodyPart}
       />
       <Exercises 
+        exercises={exercises}
         setExercises={setExercises} 
         bodyPart={bodyPart} 
         setBodyPart={setBodyPart}
